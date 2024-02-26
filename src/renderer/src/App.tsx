c38993afa769934986e4cbc0aa2b4bdf375d2b9c
Matheus Fugisaki <matheusgfmaciel@gmail.com>
2024-02-26 14:18:45 -0300

    feat: add zustand and config editor supported languages

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,35 +1,40 @@
-import { useRef, useState, useEffect } from 'react'
-import * as monaco from 'monaco-editor'
-import { getHighlighter, bundledThemesInfo, bundledLanguagesInfo } from 'shiki'
 import { shikiToMonaco } from '@shikijs/monaco'
+import * as monaco from 'monaco-editor'
+import { useEffect, useRef, useState } from 'react'
+import { getHighlighter } from 'shiki'
+import { supportedLanguages } from './shared/constants/languages'
 
 export const App = (): JSX.Element => {
   const [editor, setEditor] = useState<monaco.editor.IStandaloneCodeEditor | null>(null)
   const monacoEl = useRef(null)
 
   useEffect(() => {
-    console.log(bundledLanguagesInfo)
-    console.log(bundledThemesInfo)
-
     const loadEditor = async (): Promise<void> => {
       const highlighter = await getHighlighter({
-        themes: ['min-dark', 'tokyo-night', 'catppuccin-mocha', 'dracula'],
-        langs: ['javascript', 'typescript']
+        themes: ['min-dark', 'tokyo-night', 'dracula', 'github-dark', 'catppuccin-mocha'],
+        langs: supportedLanguages
       })
 
-      monaco.languages.register({ id: 'typescript' })
-      monaco.languages.register({ id: 'javascript' })
+      supportedLanguages.forEach((lang) => {
+        monaco.languages.register({ id: lang })
+      })
 
       shikiToMonaco(highlighter, monaco)
 
       if (monacoEl) {
         setEditor((editor) => {
-          if (editor) return editor
+          // if (editor) return editor
 
           return monaco.editor.create(monacoEl.current!, {
             value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
             language: 'typescript',
-            theme: 'dracula'
+            theme: 'dracula',
+            fontFamily: 'JetBrains Mono',
+            fontLigatures: true,
+            fontSize: 14,
+            bracketPairColorization: { enabled: true, independentColorPoolPerBracketType: true },
+            showFoldingControls: 'always',
+            automaticLayout: true
           })
         })
       }
---
+++ src/renderer/src/shared/constants/languages.ts
@@ -0,0 +1,20 @@
+import { BundledLanguage } from 'shiki/langs'
+
+export const supportedLanguages: BundledLanguage[] = [
+  'javascript',
+  'typescript',
+  'json',
+  'html',
+  'css',
+  'angular-ts',
+  'angular-html',
+  'vue',
+  'vue-html',
+  'css',
+  'scss',
+  'less',
+  'stylus',
+  'tsx',
+  'jsx',
+  'graphql'
+]
